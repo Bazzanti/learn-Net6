181769788ad678738ea3315c507b7f9ff9835937
michael.bazzanti <michael.bazzanti-ext@prada.com>
2021-11-23 12:13:30 +0100

    remove warnings context

--- FormazioneNET6/EmployeeContext.cs
+++ FormazioneNET6/EmployeeContext.cs
@@ -10,17 +10,17 @@ namespace FormazioneNET6
         
         }
 
-        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Employee> Employees { get; set; } = null!; //instance but it's not gonna be null
     }
 
     [Table("TB_EMPLOYEE", Schema = "dbo")]
     public class Employee
     {
         public int Id { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Ufficio { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Ufficio { get; set; }
 
     }
 }
